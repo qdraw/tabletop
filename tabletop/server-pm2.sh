01e2772cf937b4750b8f8857342f3e278d0705c6
Dion <dionvanvelde@gmail.com>
2018-01-08 14:47:15 +0100

    location

--- tabletop/server-pm2.sh
+++ tabletop/server-pm2.sh
@@ -5,7 +5,6 @@ export ASPNETCORE_URLS="http://localhost:5145/"
 #export ASPNETCORE_URLS="http://*:5123"
 export ASPNETCORE_ENVIRONMENT="Production"
 
-cd tabletop
-chmod +x /bin/Debug/netcoreapp2.0/linux-arm/tabletop
-pm2 start --name tabletop ./bin/Debug/netcoreapp2.0/linux-arm/tabletop
+cd tabletop/bin/Debug/netcoreapp2.0/
+pm2 start --name tabletop ./linux-arm/tabletop
 echo "tabletop started"
