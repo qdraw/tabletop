dfd0f4e0e503bda88a1ed6816ee654000fbd6fea
Dion <dionvanvelde@gmail.com>
2018-01-08 14:40:54 +0100

    update build scripts

--- tabletop/publish-arm.sh
+++ tabletop/publish-arm.sh
@@ -5,3 +5,4 @@ rm -rf bin
 dotnet restore
 dotnet build
 dotnet publish -r linux-arm
+#dotnet publish -r osx.10.11-x64
--- tabletop/server-pm2.sh
+++ tabletop/server-pm2.sh
@@ -6,10 +6,6 @@ export ASPNETCORE_URLS="http://localhost:5145/"
 export ASPNETCORE_ENVIRONMENT="Production"
 
 cd tabletop
-#hack to allow to do this multiple times
-#rm -rf obj
-#dotnet restore
-#dotnet build
-pm2 start --name tabletop dotnet -- run
-
+chmod +x /bin/Debug/netcoreapp2.0/linux-arm/tabletop
+pm2 start --name tabletop ./bin/Debug/netcoreapp2.0/linux-arm/tabletop
 echo "tabletop started"
