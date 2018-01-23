95fd92a4ba4db18937fb4e0c675aca72285531bc
Dion <dionvanvelde@gmail.com>
2018-01-23 23:20:09 +0100

    add tooltip ++ update margins ++ move out / mouse over feature

--- tabletop/tabletop/wwwroot/js/draw.js
+++ tabletop/tabletop/wwwroot/js/draw.js
@@ -16,6 +16,7 @@ var draw = {
     pageLoaded: false,
 
     index : function () {
+
         draw.start();
 
         var updateInterval = window.drawEnv.updateInterval;
@@ -118,18 +119,18 @@ var draw = {
         }
         console.log(data);
 
-       var margin = {top: 40, right: 0, bottom: 30, left: 30},
+       var margin = {top: 25, right: 0, bottom: 30, left: 30},
        width = 800 + (margin.right - margin.left),
        height = 300 + (margin.top - margin.bottom);
 
        d3.select("#data")
            .attr("preserveAspectRatio", "xMinYMin meet")
-           .attr("viewBox", "0 0 " + (width + margin.left + margin.right+5) + " " + (height +margin.top));
+           .attr("viewBox", "0 0 " + (width + margin.left + margin.right+5) + " " + (height +margin.top+15));
 
        var bars = d3.select("#data .bars")
            // .attr("height", height + margin.top + margin.bottom)
            .attr("fill", "#000")
-           .attr("transform","translate(" + (margin.left+15) + "," + 0 + ")")
+           .attr("transform","translate(" + (margin.left+20) + "," + 0 + ")")
            .selectAll("rect")
            .data(data);
 
@@ -153,8 +154,7 @@ var draw = {
        svg = d3.select("#data");
        svg.append("g")
            .attr("class", "x_time")
-           .attr("transform", "translate("+ (margin.left+5) + "," + height + ")")
-           .attr("stroke", "#000")
+           .attr("transform", "translate("+ (margin.left+5) + "," + (height+5) + ")")
            .call(
                d3.axisBottom(x)
                 .tickFormat(function(d,i) {
@@ -182,10 +182,12 @@ var draw = {
 
         svg.append("g")
             .attr("class", "y_events")
-            .attr("transform", "translate("+ (margin.left+3) + "," + 0 + ")")
-            .attr("stroke", "#000")
-            .call(d3.axisLeft(y));
+            .attr("transform", "translate("+ (margin.left+3) + "," + 5 + ")")
+            .call(
+                d3.axisLeft(y)
+            );
 
+       var tooltip = d3.select(".tooltip");
 
        // enter and update selection
        bars
@@ -193,9 +195,31 @@ var draw = {
          .merge(bars)
          .attr("x", function(d) { return x(d.label); })
          .attr("width", x.bandwidth())
-         .attr("y", function(d) { return y(d.weight); })
-         .attr("height", function(d) { return height - y(d.weight); });
-         // .text(function (d) {return d;});
+         .attr("y", function(d) { return y(d.weight)+5; })
+         .attr("height", function(d) { return height - y(d.weight); })
+         .on("mousemove", function(d){
+             console.log("sdf");
+             d3.select(this).attr("fill", "#588C73");
+
+             var offset = new Date().getTimezoneOffset() / 60 * -1;
+             var startTime = Number(d.label.substr(0, 2)) + offset + ":" + d.label.substr(3, 2);
+             var endTime = Number(d.label.substr(0, 2)) + offset + ":" + draw.leadingZero(Number(d.label.substr(3, 2))+5);
+
+            tooltip
+              .style("left", d3.event.pageX - 50 + "px")
+              .style("top", d3.event.pageY - 60 + "px")
+              .style("display", "inline-block")
+              .html((startTime) + "-" + endTime + "<br>" + "Events: " + (d.weight) + "x");
+          })
+    	  .on("mouseout", function(d){
+              d3.select(this).attr("fill", function() {
+                 return "";
+              });
+
+              tooltip.style("display", "none");
+          });
+         // .on("click", clicked);
+         // .text(function (d) {return d.label;});
 
        // exit selection
        bars
@@ -208,9 +232,13 @@ var draw = {
        }
 
        draw.pageLoaded = true;
-
+       console.log();
 
     }
 };
+
+function clicked(d) {
+    console.log(d);
+}
 
 draw.index();
